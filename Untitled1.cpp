1180d15fdc5fcd0ebe85a986ced417e8f357922d
HoangTuan12356 <hoangxuantuan1996@gmail.com>
2017-05-29 08:33:22 +0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float a, b, c, T;
+	cout<< "Nhap vao 3 so: "; cin >> a >>b>>c;
+	T = (a+b+c)/3;
+	cout<< "Gia tri trung binh 3 so vua nhap la: "<< T;
+	system "pause";
+}
