1703f6dc34baf3e175a5fd9d684ba2e7378f0a2a
HoangTuan12356 <hoangxuantuan1996@gmail.com>
2017-05-29 08:34:45 +0700

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -6,5 +6,6 @@ int main()
 	cout<< "Nhap vao 3 so: "; cin >> a >>b>>c;
 	T = (a+b+c)/3;
 	cout<< "Gia tri trung binh 3 so vua nhap la: "<< T;
+	cout<< "Tong ba so vua nhap vao la: "<< a+b+c;
 	system "pause";
 }
